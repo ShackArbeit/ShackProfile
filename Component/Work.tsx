5086dd46d7a2888e2e1ca21c37f853ef2e23ceb1
Shack Lin <g0972222165@gmail.com>
2024-03-01 15:00:40 +0800

    Add MapData

--- Component/Work.tsx
+++ Component/Work.tsx
@@ -1,25 +1,50 @@
+'use client'
+import {useState,useEffect} from 'react'
+
+
+
+interface profileData{
+  name:string,
+  title:string,
+  url:string,
+  Date:string,
+  period:string
+}
 
 
 const Work = () => {
+  const[datas,setDatas]=useState([])
+  useEffect(()=>{
+       const fetchData=async()=>{
+          const response=await fetch('/api/work')
+          console.log(response)
+          const Data=await response.json()
+          setDatas(Data)
+      }
+      fetchData()
+  },[])
   return (
-    <section
-    className='bg-workColor py-[40px] px-[20px] dark:bg-black'
-    >
-        <h1 className='mb-[20px] text-4xl text-center font-bold'>Work Experience </h1>
-        <div className='max-w-[500px] border-l-2 border-black m-auto pl-[32px] dark:border-l-2 dark:border-white '>
-        <div className='job'>
-            <h1 className='text-xl'>Trend-Go</h1>
-            <h3 className='text-base text-[#9c27b0]'>Frontend Enginer</h3>
-            <img src='https://media.graphassets.com/noBoILheTnNANM7wZvQ2' 
-               alt='trend_go' 
-               className='absolute top-[30px] right-[10px] object-contain h-[40px] w-[90px]'/>
-            <div className='absolute font-bold left-[-125px] top-[50%]  translate-y-2/4 
-                            dark:text-white'>Oct 5 2023</div>
-        </div>
-        </div>
-     
-     
-    </section>
+    <>
+      <section
+      className='bg-workColor py-[40px] px-[20px] dark:bg-black'
+      >
+          <h1 className='mb-[20px] text-4xl text-center font-bold'>Work Experience </h1>
+          <div className='max-w-[500px] border-l-2 border-black m-auto pl-[32px] dark:border-l-2 dark:border-white '>
+          {datas.map((data:profileData)=>(
+          <div className='job'>
+              <h1 className='text-xl'>{data.name}</h1>
+              <h3 className='text-base text-[#9c27b0] font-bold my-4'>{data.title}</h3>
+              <h3 className='text-base text-black '>{data.period}</h3>
+              <img src={data.url}
+                 alt='trend_go' 
+                 className='absolute top-[50px] right-[10px] object-contain h-[40px] w-[90px]'/>
+              <div className='absolute font-bold left-[-140px] top-[30%]  translate-y-2/4 
+                              dark:text-white'>{data.Date}</div>
+          </div>
+          ))}
+          </div>
+      </section>
+   </>
   )
 }
 
