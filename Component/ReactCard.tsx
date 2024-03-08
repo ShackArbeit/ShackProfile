42c02acd60c402c2220726dc76ad8c22dfa7b425
Shack Lin <g0972222165@gmail.com>
2024-03-08 15:35:08 +0800

    Add Api Content

--- Component/ReactCard.tsx
+++ Component/ReactCard.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState, useEffect } from 'react';
 interface ReactContent {
+  id:number,
   title: string;
   imgurl: string;
   dec: string;
@@ -16,8 +17,7 @@ interface SpanContent {
 
 const ReactCard = () => {
   const [datas, setDatas] = useState<ReactContent[]>([]);
-  const [spans, setSpans] = useState<SpanContent | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [spans, setSpans] = useState<SpanContent []>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,45 +34,38 @@ const ReactCard = () => {
       const spanResponse = await fetch('/api/ReactProject/DecSpan');
       const spanData = await spanResponse.json();
       setSpans(spanData);
-      setLoading(false); // 設置載入狀態為 false
     };
-
     fetchSpanData();
   }, []);
-
-  if (loading) {
-    return <div>載入中...</div>;
-  }
 
   console.log(spans);
 
   return (
     <section className='flex flex-row border-2 border-red justify-center items-center flex-wrap gap-y-8 gap-x-5'>
-      {datas.map((data: ReactContent) => (
-        <div className='p-4 border-1 border-[#6b7688] text-center bg-[#e0e0e0] w-[280px] rounded-xl hover:shadow-[0_5px_50px_-20px_#0eb9f5]' key={data.title}>
-          <>
-            <Image width={250} height={0} alt='' src={data.imgurl} />
-            <p className='mt-5 mb-2.5 text-2xl font-semibold'>{data.title}</p>
-            <div className='mb-4 box-border text-center w-full border-2 border-red-900 '>
-              {spans && spans.content && spans.content.map((span: string) => (
-                <span key={span} className='p-1 font-semibold text-[12px] border-2 border-[#9c27b0] rounded-md inline-flex text-[#9c27b0] mx-1 my-1'>
-                  {span}
-                </span>
-              ))}
-            </div>
-            <p className='text-base tracking-widest'>{data.dec}</p>
-            <div className='mt-5 box-border flex justify-center '>
-              <a className='block cursor-pointer h-[50px] w-[50px] mx-5' href={data.giturl} target="_blank">
-                <Image src='/Image/github.png' alt='github' width={0} height={0} sizes="100vw " style={{ width: '100%', height: '100%', borderRadius: '50%', border: '1px solid black' }} />
-              </a>
-              <a className='block h-[50px] w-[50px] cursor-pointer mx-5' href={data.demourl} target="_blank">
-                <Image src='/Image/demo.jpg' alt='web' width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', borderRadius: '50%', border: '1px solid black' }} />
-              </a>
-            </div>
-          </>
-        </div>
-      ))}
-    </section>
+    {datas.map((data: ReactContent) => (
+      <div className='p-4 border-1 border-[#6b7688] text-center bg-[#e0e0e0] w-[280px] rounded-xl hover:shadow-[0_5px_50px_-20px_#0eb9f5]' key={data.title}>
+        <>
+          <Image width={250} height={0} alt='' src={data.imgurl} />
+          <p className='mt-5 mb-2.5 text-2xl font-semibold'>{data.title}</p>
+          <div className='mb-4 box-border text-center w-full border-2 border-red-900 '>
+            {/* 在這裡遍歷 spans，找到相應的 spans */}
+            {spans.find((Span: SpanContent) => Span.id === data.id)?.content.map((span) => (
+              <span className='p-1 font-semibold text-[12px] border-2 border-[#9c27b0] rounded-md inline-flex text-[#9c27b0] mx-1 my-1' key={span}>{span}</span>
+            ))}
+          </div>
+          <p className='text-base tracking-widest'>{data.dec}</p>
+          <div className='mt-5 box-border flex justify-center '>
+            <a className='block cursor-pointer h-[50px] w-[50px] mx-5' href={data.giturl} target="_blank">
+              <Image src='/Image/github.png' alt='github' width={0} height={0} sizes="100vw " style={{ width: '100%', height: '100%', borderRadius: '50%', border: '1px solid black' }} />
+            </a>
+            <a className='block h-[50px] w-[50px] cursor-pointer mx-5' href={data.demourl} target="_blank">
+              <Image src='/Image/demo.jpg' alt='web' width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', borderRadius: '50%', border: '1px solid black' }} />
+            </a>
+          </div>
+        </>
+      </div>
+    ))}
+  </section>
   );
 };
 
--- app/api/ReactProject/route.ts
+++ app/api/ReactProject/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import React1 from '../../../public/Image/React1.png'
 
 const reactContent=[
-    {title:'Taiwan Food',imgurl:React1,dec:'Use ReactJs to build a static web page for Liang Shehan, a famous restaurant chain in Taiwan',giturl:'https://github.com/ShackLin/TaiwanFood-Demo',demourl:'https://taiwanfood-demo.pages.dev/'},
-    {title:'Taiwan Food2',imgurl:React1,dec:'Use ReactJs to build a static web page for Liang Shehan, a famous restaurant chain in Taiwan',giturl:'https://github.com/ShackLin/TaiwanFood-Demo',demourl:'https://taiwanfood-demo.pages.dev/'}
+    {id:1,title:'Taiwan Food',imgurl:React1,dec:'Use ReactJs to build a static web page for Liang Shehan, a famous restaurant chain in Taiwan',giturl:'https://github.com/ShackLin/TaiwanFood-Demo',demourl:'https://taiwanfood-demo.pages.dev/'},
+    {id:2,title:'Taiwan Food2',imgurl:React1,dec:'Use ReactJs to build a static web page for Liang Shehan, a famous restaurant chain in Taiwan',giturl:'https://github.com/ShackLin/TaiwanFood-Demo',demourl:'https://taiwanfood-demo.pages.dev/'}
 ]
 
 
